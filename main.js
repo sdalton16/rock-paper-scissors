0b5b4db0f1854cd4abe4ed1cddebadf56fd1f472
sdalton16 <136284067+sdalton16@users.noreply.github.com>
2023-12-09 16:15:50 -0700

    Implemented UI
    UI added to allow player to play game via UI vs. console
    UI includes scoreboard and logic to declare game over

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
 let comp_selection;
 let user_selection;
-let winner;
+let winner = 3;
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+let user_score = document.querySelector('#user_score');
+let comp_score = document.querySelector('#comp_score');
+let user_counter = 0;
+let comp_counter = 0;
 
+const main = document.querySelector('#main');
+const game_result = document.querySelector('#game_result');
+const restart = document.querySelector('#restart');
+game_result.hidden = true;
+restart.hidden = true;
+
+restart.addEventListener('click', () => location.reload());
+
+rock.addEventListener('click', () => {
+    user_selection = 1;
+    return round(user_selection);
+});
+
+paper.addEventListener('click', () => {
+    user_selection = 2;
+    return round(user_selection);
+});
+
+scissors.addEventListener('click', () => {
+    user_selection = 3;
+    return round(user_selection);
+});
 // user_win = 1
-//comp_win = 0
+// comp_win = 0
 // rock = 1
 // paper = 2
 // scissors = 3
-
 
 // Randomly assign computer choice for each round, called in round function
 const getCompSelection = function () {
@@ -16,102 +44,95 @@ const getCompSelection = function () {
     return comp_selection;
 }
 
-// Prompts user for their selection for each round, called in round function
-const getUserSelection = function () {
-    user_selection = prompt('Rock, Paper, or Scissors? ');
-
-    if (user_selection.toLowerCase() === 'rock') {
-        user_selection = 1;
-    }
-
-    else if (user_selection.toLowerCase() === 'paper') {
-        user_selection = 2;
-    }
-
-    else if (user_selection.toLowerCase() === 'scissors') {
-        user_selection = 3;
-    }
-
-    else {
-        return getUserSelection();
-    }
-
-    return user_selection;
-}
-
 // Evaluates each round and returns winner or calls iteself if tie, called
-// game function
-const round = function () {
+// in game function
+const round = function (user_selection) {
     comp_selection = getCompSelection();
-    user_selection = getUserSelection();
 
     if (comp_selection === 1 && user_selection === 3) {
         winner = 0;
         console.log('Computer wins round - Rock beats scissors :(');
-        return winner;
+        result.textContent = 'Computer wins round - Rock beats scissors :(';
+        comp_counter += 1;
+        comp_score.textContent = comp_counter;
+        return checkForWinner();
     }
 
     else if (comp_selection === 2 && user_selection === 1) {
         winner = 0;
         console.log('Computer wins round - Paper beats rock :(');
-        return winner;
+        result.textContent = 'Computer wins round - Paper beats rock :(';
+        comp_counter += 1;
+        comp_score.textContent = comp_counter;
+        return checkForWinner();
     }
 
     else if (comp_selection === 3 && user_selection === 2) {
         winner = 0;
         console.log('Computer wins round - Scissors beats paper :(');
-        return winner;
+        result.textContent = 'Computer wins round - Scissors beats paper :(';
+        comp_counter += 1;
+        comp_score.textContent = comp_counter;
+        return checkForWinner();
     }
 
     else if (user_selection === 1 && comp_selection === 3) {
         winner = 1;
         console.log('User wins round - Rock beats scissors :)');
-        return winner;
+        result.textContent = 'User wins round - Rock beats scissors :)';
+        user_counter += 1;
+        user_score.textContent = user_counter;
+        return checkForWinner();
     }
 
     else if (user_selection === 2 && comp_selection === 1) {
         winner = 1;
         console.log('User wins round - Paper beats rock :)');
-        return winner;
+        result.textContent = 'User wins round - Paper beats rock :)';
+        user_counter += 1;
+        user_score.textContent = user_counter;
+        return checkForWinner();
     }
 
     else if (user_selection === 3 && comp_selection === 2) {
         winner = 1;
         console.log('User wins round - Scissors beats paper :)');
-        return winner;
+        result.textContent = 'User wins round - Scissors beats paper :)';
+        user_counter += 1;
+        user_score.textContent = user_counter;
+        return checkForWinner();
     }
 
     else if (user_selection === comp_selection) {
         console.log('Tie...Play Again');
+        result.textContent = 'Tie...Play Again';
         return round();
     }
 }
 
-// primary function to run game
-const game = function () {
-    let comp_score = 0;
-    let user_score = 0;
+const checkForWinner = function () {
 
-    for (i = 0; i < 5; i++) {
-        winner = round();
-        if (winner === 0) {
-            comp_score++;
-            if (comp_score === 3) {
-                return console.log('Computer wins game :(')
-            }
+    if (comp_counter === 5 || user_counter === 5) {
+        main.hidden = true;
+
+        if (comp_counter === 5) {
+            game_result.hidden = false;
+            restart.hidden = false;
+            comp_counter = 0;
+            user_counter = 0;
+            game_result.textContent = 'Computer wins game :(';
+            return 'Game over - comp win';
         }
 
-        else if (winner === 1) {
-            user_score++;
-            if (user_score === 3) {
-                return console.log('User wins game :)')
-            }
+        if (user_counter === 5) {
+            game_result.hidden = false;
+            restart.hidden = false;
+            comp_counter = 0;
+            user_counter = 0;
+            game_result.textContent = 'Computer wins game :(';
+            return 'Game over - user win';
         }
-
-        else {
-            return console.log('Invalid round result');
-        }
-    }
-
-    return console.log('Invalid game result');
-}
+        return 'Continue game';
+    };
+    return 'Continue game';
+};
